03f50c4ee48d0af5eb1e8428e93ef1b4a92c0b18
Snehal Patil <sneha.swapnil1996@gmail.com>
2021-10-30 18:40:59 +0530

    Created table the address book

---
+++ Program.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace AddressBookWithLinq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the address book using linq");
+            AddressBookTable addressBook = new AddressBookTable();
+            addressBook.createTable();
+        }
+    }
+}
